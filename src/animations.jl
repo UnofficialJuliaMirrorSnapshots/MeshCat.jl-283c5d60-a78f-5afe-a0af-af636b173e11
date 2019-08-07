dbe17d100de0ab782fc734757fa15895345e8612
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-07 14:29:37 +0000

    Snapshot of branch master taken on 2019-08-07T10:29:36.252-04:00 from "https://github.com/UnofficialJuliaMirror/MeshCat.jl-283c5d60-a78f-5afe-a0af-af636b173e11"

--- src/animations.jl
+++ src/animations.jl
@@ -2,8 +2,34 @@
 struct AnimationTrack{T}
     name::String
     jstype::String
-    frames::Vector{Int}
-    values::Vector{T}
+    events::Vector{Pair{Int, T}} # frame => value
+end
+
+AnimationTrack{T}(name::String, jstype::String) where {T} = AnimationTrack(name, jstype, Pair{Int, T}[])
+
+function Base.insert!(track::AnimationTrack, frame::Integer, value)
+    i = searchsortedfirst(track.events, frame; by=first)
+    if i <= length(track.events) && first(track.events[i]) == frame
+        track.events[i] = frame => value
+    else
+        insert!(track.events, i, frame => value)
+    end
+    return track
+end
+
+function Base.merge!(a::AnimationTrack{T}, others::AnimationTrack{T}...) where T
+    l = length(a.events)
+    for other in others
+        @assert other.name == a.name
+        @assert other.jstype == a.jstype
+        l += length(other.events)
+    end
+    events = similar(a.events, l)
+    mergesorted!(events, a.events, Iterators.flatten((other.events for other in others)))
+    combine!(events, by=first, combine=(a, b) -> b)
+    resize!(a.events, length(events))
+    copyto!(a.events, events)
+    return a
 end
 
 @with_kw struct AnimationClip
@@ -12,12 +38,30 @@ end
     name::String = "default"
 end
 
+function Base.merge!(a::AnimationClip, others::AnimationClip...)
+    for other in others
+        @assert other.fps == a.fps
+        merge!(merge!, a.tracks, other.tracks) # merge tracks recursively
+    end
+    return a
+end
+
 struct Animation
     clips::Dict{Path, AnimationClip}
     default_framerate::Int
 end
 
 Animation(fps::Int=30) = Animation(Dict{Path, AnimationClip}(), fps)
+
+function Base.merge!(a::Animation, others::Animation...)
+    for other in others
+        @assert a.default_framerate == other.default_framerate
+        merge!(merge!, a.clips, other.clips) # merge clips recursively
+    end
+    return a
+end
+
+Base.merge(a::Animation, others::Animation...) = merge!(Animation(a.default_framerate), a, others...)
 
 function convert_frames_to_video(tar_file_path::AbstractString, output_path::AbstractString="output.mp4"; framerate=60, overwrite=false)
     output_path = abspath(output_path)
